b4c50b356ab08f3fd665316464f44b93ac575698
Jaakko Tulkki <jaakko.tulkki.npm@gmail.com>
2025-09-21 11:58:26 +0100

    added sqs and writing to it

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
+const sqs = new SQSClient({ region: "eu-west-1" });
+const QUEUE_URL = process.env.QUEUE_URL;
+
 exports.handler = async (event) => {
-  console.log("Event:", event);
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Hello from Lambda Function URL!" }),
-  };
+  console.log("Incoming event:", event);
+
+  try {
+    const body = event.body ? JSON.parse(event.body) : { message: "no body" };
+
+    const cmd = new SendMessageCommand({
+      QueueUrl: QUEUE_URL,
+      MessageBody: JSON.stringify(body),
+    });
+
+    await sqs.send(cmd);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ status: "Message sent to SQS", payload: body }),
+    };
+  } catch (err) {
+    console.error("Error sending to SQS:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Failed to send message",
+        details: err.message,
+      }),
+    };
+  }
 };
--- main.tf
+++ main.tf
@@ -38,13 +38,62 @@ resource "aws_lambda_function" "hello" {
 
   filename         = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      QUEUE_URL = aws_sqs_queue.hello_queue.id
+    }
+  }
 }
+
 
 resource "aws_lambda_function_url" "hello_url" {
   function_name      = aws_lambda_function.hello.function_name
   authorization_type = "NONE"
 }
 
+# Dead Letter Queue
+resource "aws_sqs_queue" "hello_dlq" {
+  name                      = "hello-dlq"
+  message_retention_seconds = 1209600 # 14 days (max)
+}
+
+# Main SQS Queue
+resource "aws_sqs_queue" "hello_queue" {
+  name                       = "hello-queue"
+  visibility_timeout_seconds = 30
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.hello_dlq.arn
+    maxReceiveCount     = 3
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_sqs_send" {
+  name = "lambda-sqs-send"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "sqs:SendMessage"
+      Resource = aws_sqs_queue.hello_queue.arn
+    }]
+  })
+}
+
+
+
+
+
 output "function_url" {
   value = aws_lambda_function_url.hello_url.function_url
+}
+
+output "sqs_queue_url" {
+  value = aws_sqs_queue.hello_queue.id
+}
+
+output "sqs_dlq_url" {
+  value = aws_sqs_queue.hello_dlq.id
 }
